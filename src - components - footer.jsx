c0005468408803d4448fc158301d3585fa8e56e0
Ibrahim-anw <ibrahimanwar9900@gmail.com>
2026-09-26 00:08:15 +0600

    src - components - footer.jsx

---
+++ src - components - footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>© {new Date().getFullYear()} Syed Ibrahim Anwar</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
