417491a3e550b7026655f1e6ebe66696f0a256e4
Ibrahim-anw <ibrahimanwar9900@gmail.com>
2026-09-26 00:22:24 +0600

    src - components - app.jsx

---
+++ src - components - app.jsx
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import Skills from './components/Skills.jsx'
+import Experience from './components/Experience.jsx'
+import Education from './components/Education.jsx'
+import Contact from './components/Contact.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Experience />
+      <Education />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
